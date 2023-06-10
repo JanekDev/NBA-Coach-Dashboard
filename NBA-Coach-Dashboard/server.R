509ab1da34445fdcf20fb2a0517cd835589b3b8d
LukaszSztukiewicz <sztukiewicz.lukasz@gmail.com>
2023-06-10 13:01:17 -0400

    UI skeleton

--- NBA-Coach-Dashboard/server.R
+++ NBA-Coach-Dashboard/server.R
@@ -1,35 +1,49 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(nbastatR)
 
-# Define server logic required to draw a histogram
+getPlayersStats <- function (){
+  players_stats = bref_players_stats(
+    tables = c("advanced", "per_game"),
+    seasons = 2023,
+    assign_to_environment = FALSE
+  )
+  relevant_cols = c("namePlayer","minutes", "slugPosition", "agePlayer", "slugTeamBREF", "countGames", "isHOFPlayer", "slugTeamsBREF", "urlPlayerHeadshot", "ratioPER", "trbPerGame", "astPerGame", "stlPerGame", "blkPerGame", "ptsPerGame")
+  # get only players with minutes > 144
+  players_stats = players_stats[players_stats$minutes > 144, relevant_cols]
+  return(players_stats)
+}
+
 function(input, output, session) {
-  output$table <- renderTable({
-    # Fetch the data for the selected team and season
-    team_data <- teams_rosters(team = input$team, season = input$season)
-    
-    # Perform the ranking based on the selected option
-    if (input$option == "Tall") {
-      team_data <- team_data[order(-team_data$height),]
-    } else if (input$option == "Best") {
-      # For "Best" option, you might want to consider a parameter like player efficiency rating (PER)
-      # Let's assume we have a column named 'PER' in the dataset
-      team_data <- team_data[order(-team_data$PER),]
-    } else if (input$option == "Young") {
-      team_data <- team_data[order(team_data$age),]
+
+  players_stats = getPlayersStats()
+  
+  output$select_team <- renderUI(
+    {
+      selectInput(
+        "select_team",
+        "Select Team",
+        unique(players_stats$slugTeamBREF)
+      )
     }
-    
-    # Get the top 5 players
-    team_data <- head(team_data, 5)
-    return(team_data)
+  )
+  
+  output$table <- renderTable({
+   
+    # # Perform the ranking based on the selected option
+    # if (input$option == "Tall") {
+    #   team_data <- team_data[order(-team_data$height),]
+    # } else if (input$option == "Best") {
+    #   # For "Best" option, you might want to consider a parameter like player efficiency rating (PER)
+    #   # Let's assume we have a column named 'PER' in the dataset
+    #   team_data <- team_data[order(-team_data$PER),]
+    # } else if (input$option == "Young") {
+    #   team_data <- team_data[order(team_data$age),]
+    # }
+    # 
+    # # Get the top 5 players
+    # team_data <- head(team_data, 5)
+    # return(team_data)
+    return(filter(players_stats, slugTeamBREF=="TOT"))
   })
 }
 
--- NBA-Coach-Dashboard/ui.R
+++ NBA-Coach-Dashboard/ui.R
@@ -1,27 +1,39 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(plotly)
+library(flexdashboard)
 
-teams = nba_teams_seasons()
 
-fluidPage(
-  titlePanel("NBA Starting 5 Advisor"),
+
+startingFivePage <- function(){
   sidebarLayout(
     sidebarPanel(
-      selectInput("team", "Choose a Team:", choices = unique(nba_teams$nickname)),
-      selectInput("season", "Choose a Season:", choices = unique(seasons$season)),
-      selectInput("option", "Choose an Option:", choices = c("Tall", "Best", "Young"))
+      uiOutput("select_team"),
+      actionButton("update", "Change"),
+      hr()
     ),
     mainPanel(
       tableOutput("table")
     )
   )
+}
+
+playerMatchupPage <- function(){}
+draftPicksPage <- function(){}
+aboutPage <- function(){}
+
+shinyUI(
+  fluidPage(
+    navbarPage(
+      "NBA Coach Dashboard",
+      theme = shinytheme("sandstone"),
+      tabPanel("Starting 5", startingFivePage()),
+      tabPanel("Player matchup", playerMatchupPage()),
+      tabPanel("Draft Picks", draftPicksPage()),
+      tabPanel("About", aboutPage())
+    )
+  )
 )
+
 
